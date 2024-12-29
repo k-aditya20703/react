e5a1fe9020a4dac47fa83d7d2d4416e7eec54422
Gagan's_github <65104778+Gagangithub1988@users.noreply.github.com>
2024-12-29 20:59:52 +0530

    Revert "loading color changed"

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -63,7 +63,7 @@ const searchData = pokemonData.filter((curPokemon)=>
         <h1>Let's Catch Pokemon</h1>
 
         <input type='search' value={search} onChange={handleSearch} placeholder='Search Pokemon'></input>
-        {isLoading &&<p style={{color:'red'}}>Loading...</p>}
+        {isLoading &&<p>Loading...</p>}
         </div>
         {
           //pokemonData.map((curPokemon)=>{
